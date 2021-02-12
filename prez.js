b1a1d0cb36953bd8f11c82c9408adaf9d23b0cd4
nass92 <goujanassim@hotmail.com>
2021-02-12 15:24:34 +0100

    test

--- prez.js
+++ prez.js
@@ -1,5 +1,5 @@
 let firstname = "Sofiane"
 let age = 39
 let city = "Paris"
-
-console.log(`Hello my name is ${firstname} and I'm  ${age} years old. I live in ${city}.`)
+let msg = `Hello my name is ${firstname} and I'm  ${age} years old. I live in ${city}.`
+console.log(msg)
